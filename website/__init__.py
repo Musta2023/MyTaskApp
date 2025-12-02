1004dbce71fd4be3b782bdf252bca9ac1af9e157
mustapha <mustaphalaaraj1995@gmail.com>
2025-12-02 11:34:14 +0100

    deployement beugs

--- website/__init__.py
+++ website/__init__.py
@@ -22,37 +22,6 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-    
-    with app.app_context():
-        db.create_all()
-        # Simple migration: add columns if they don't exist (SQLite-safe)
-        try:
-            from sqlalchemy import text
-            with db.engine.connect() as conn:
-                # Note table migrations
-                cols = [row[1] for row in conn.execute(text("PRAGMA table_info('note')"))]
-                if 'status' not in cols:
-                    conn.execute(text("ALTER TABLE note ADD COLUMN status VARCHAR(20) DEFAULT 'planned'"))
-                if 'pomodoros' not in cols:
-                    conn.execute(text("ALTER TABLE note ADD COLUMN pomodoros INTEGER DEFAULT 0"))
-                if 'pomodoro_seconds' not in cols:
-                    conn.execute(text("ALTER TABLE note ADD COLUMN pomodoro_seconds INTEGER DEFAULT 1500"))
-                if 'finish_by' not in cols:
-                    conn.execute(text("ALTER TABLE note ADD COLUMN finish_by DATETIME"))
-                
-                # PomodoroSession table migrations
-                sess_cols = [row[1] for row in conn.execute(text("PRAGMA table_info('pomodoro_session')"))]
-                if 'target_end_at' not in sess_cols:
-                    conn.execute(text("ALTER TABLE pomodoro_session ADD COLUMN target_end_at DATETIME"))
-                if 'is_paused' not in sess_cols:
-                    conn.execute(text("ALTER TABLE pomodoro_session ADD COLUMN is_paused BOOLEAN DEFAULT 0"))
-                if 'remaining_seconds' not in sess_cols:
-                    conn.execute(text("ALTER TABLE pomodoro_session ADD COLUMN remaining_seconds INTEGER"))
-                if 'paused_at' not in sess_cols:
-                    conn.execute(text("ALTER TABLE pomodoro_session ADD COLUMN paused_at DATETIME"))
-        except Exception:
-            # Ignore migration errors in case of non-SQLite or already migrated
-            pass
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
